4b66964ab8e16be4cd0203ee68376dbf3a8077bb
Diogo Matos <diogo.s.matos@tecnico.ulisboa.pt>
2021-03-19 17:51:15 +0000

    report progress + automate octave tables

--- t1/mat/t1.m
+++ t1/mat/t1.m
@@ -32,10 +32,29 @@ Kc = 8.24825537187e3 %%Ohm
 A=[R1+R3+R4,-R3,-R4,0;-R3*Kb,R3*Kb-1,0,0;-R4,0,R4+R6+R7-Kc,0;0,0,0,1]
 B=[Va;0;0;Id]
 
-A\B
+a=A\B
 
 %%Node analysis of the voltages in the circuit
-C=[G7, 0, 0, 0, 0, 0, G6, 0; 1, -1, 0, 0, 0, 0, 0, 0; 0, 0, 0, 0, 0, 1, -1, 0; 0, 0, 0, -G2, G1+G2+G3, -G1, 0, -G3; 0, 0, G5, 0, G3, 0, G4+G6, -G3-G4-G5; 1, 0, 0, 0, 0, 0, Kc*G6, -1; 0, 0, 0, 0, G1, -G1, -G4-G6, G4; 0, 0, 0, -G2, Kb+G2, 0, 0, -Kb]
+C = [G7, 0, 0, 0, 0, 0, G6, 0; 1, -1, 0, 0, 0, 0, 0, 0; 0, 0, 0, 0, 0, 1, -1, 0; 0, 0, 0, -G2, G1+G2+G3, -G1, 0, -G3; 0, 0, G5, 0, G3, 0, G4+G6, -G3-G4-G5; 1, 0, 0, 0, 0, 0, Kc*G6, -1; 0, 0, 0, 0, G1, -G1, -G4-G6, G4; 0, 0, 0, -G2, Kb+G2, 0, 0, -Kb]
 D = [0; 0; Va; 0; Id; 0; 0; 0]
 
-C\D
+c=C\D
+
+tab=fopen("current_tab.tex","w");
+
+fprintf(tab, "@$I_x$ & %f \\\\ \\hline \n", a(1,1))
+fprintf(tab, "@$I_y$ & %f \\\\ \\hline \n", a(2,1))
+fprintf(tab, "@$I_z$ & %f \\\\ \\hline \n", a(3,1))
+fprintf(tab, "@$I_w$ & %f \\\\ \\hline \n", a(4,1))
+fclose(tab);
+
+tab=fopen("volt_tab.tex","w");
+fprintf(tab, "$V_1$ & %f \\\\ \\hline \n", c(6,1))
+fprintf(tab, "$V_2$ & %f \\\\ \\hline \n", c(5,1))
+fprintf(tab, "$V_3$ & %f \\\\ \\hline \n", c(4,1))
+fprintf(tab, "$V_4$ & %f \\\\ \\hline \n", c(7,1))
+fprintf(tab, "$V_5$ & %f \\\\ \\hline \n", c(8,1))
+fprintf(tab, "$V_6$ & %f \\\\ \\hline \n", c(3,1))
+fprintf(tab, "$V_7$ & %f \\\\ \\hline \n", c(1,1))
+fprintf(tab, "$V_8$ & %f \\\\ \\hline \n", c(2,1))
+fclose(tab);
